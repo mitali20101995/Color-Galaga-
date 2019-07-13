b7cbece14129f4b114e29e931895956b81750c93
Mitali Ahir <mitaliahir3@gmail.com>
2019-07-13 12:56:01 -0400

    Initial commit

---
+++ WearableTest/Memorize This!/Memorize This!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memorize This!
+//
+//  Created by Siddharth Trivedi on 2019-07-13.
+//  Copyright © 2019 Mitali Ahir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
